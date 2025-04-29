87247b507044cd5874b353ea42f120ac25c6afb4
abdlkaderkhatir <a.khatir@esi-sba.dz>
2025-04-29 20:23:39 +0100

    init first nestjs app

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+
+@Controller('coffees')
+export class CoffeesController {
+
+    @Get('flavors')
+    findAll() {
+        return 'This action returns all coffees';
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) { // or @Param() params : { id: string }
+        return 'This action returns a #coffee' + id;
+    }
+
+
+    @Post()
+    create(@Body() body : any) {
+        // const { name, brand } = body;
+        // return 'This action creates a new coffee';
+        return body;
+    }
+}
