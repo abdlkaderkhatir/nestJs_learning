da65fc3eede0a100470620748773dc86352f28b7
abdlkaderkhatir <a.khatir@esi-sba.dz>
2025-05-01 01:54:52 +0100

    adding validation with DTO

--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -1,11 +1,18 @@
 
 
-export class UpdateCoffeeDto {
-    // @IsString() // this is a class validator decorator that checks if the value is a string
-    // @IsNotEmpty() // this is a class validator decorator that checks if the value is not empty
-    // @IsOptional() // this is a class validator decorator that checks if the value is optional
-    readonly name?: string; // or name?: string; or name: string | undefined; or name: string | null; or name: string | undefined | null; or name: string | null | undefined; or name: string | null | undefined | '';
+// export class UpdateCoffeeDto {
+//     // @IsString() // this is a class validator decorator that checks if the value is a string
+//     // @IsNotEmpty() // this is a class validator decorator that checks if the value is not empty
+//     // @IsOptional() // this is a class validator decorator that checks if the value is optional
+//     readonly name?: string; // or id?: string; or id: string | undefined; or id: string | null; or id: string | undefined | null; or id: string | null | undefined; or id: string | null | undefined | '';
+//     readonly brand?: string;
+//     readonly flavors?: string[];
+// }
 
-    readonly brand?: string;
-    readonly flavors?: string[];
+import { PartialType } from "@nestjs/mapped-types";
+import { CreateCoffeeDto } from "./create-coffee.dto";
+
+
+export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {
+    // PartialType is a utility function that makes all properties of the class optional
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.enableCors(); // enable CORS for all routes
+  // app.enableCors({ origin: 'http://localhost:3000' }); // enable CORS for specific origin
+  // validate the request body
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  // whitelist: true will remove any properties that are not in the DTO
+  // transform: true will transform the request body to the DTO class
   await app.listen(3000);
 }
 bootstrap();
