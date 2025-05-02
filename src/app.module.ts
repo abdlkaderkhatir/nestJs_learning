6b954f92f3b93e7f49ba41601bb3ffb7f4c13415
abdlkaderkhatir <a.khatir@esi-sba.dz>
2025-05-02 01:17:29 +0100

    creating typeOrm entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,29 @@ import { AppService } from './app.service';
 import { CoffeesController } from './coffees/coffees.controller';
 import { CoffeesService } from './coffees/coffees.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [CoffeesModule , TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'pass123',
+    database: 'coffee',
+    // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    autoLoadEntities: true, // Automatically load entities from the modules imported in the application in another way 
+    // synchronize: true, // Automatically synchronize the database schema with the entities
+    synchronize: true,
+    // logging: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+
+// give me command for installing the package class-validator and class-transformer
+// npm install class-validator class-transformer --save
+// give me command for installing  type orm and postgres
+// npm install @nestjs/typeorm typeorm pg --save
--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 import { CoffeesController } from './coffees.controller';
+import { Type } from 'class-transformer';
+import { Coffee } from './entities/coffee.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 // nest g mo coffees --no-spec
 // the command above will create a module and a spec file for the module
 @Module({
-    imports: [],
+    imports: [TypeOrmModule.forFeature([Coffee])], // Import the TypeOrmModule and register the Coffee entity
     controllers: [CoffeesController],
     providers: [CoffeesService],
     exports: [],
